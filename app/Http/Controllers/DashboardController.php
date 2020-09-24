461fd735f300834eb5c9cf68c8770ac6f45972cc
Brian K. Kiragu <brian@amprest.co.ke>
2020-09-24 12:20:54 +0300

    Started add typescript support

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+    /**
+     * Show the user dashboard.
+     *
+     * @author Brian K. Kiragu <brian@amprest.co.ke>
+     */
+    public function dashboard(Request $request)
+    {
+        return Inertia::render('Dashboard');
+    }
+}
