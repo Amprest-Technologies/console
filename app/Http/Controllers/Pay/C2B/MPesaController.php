a618457a043347b30fbd297170e0d034f337cdf8
Brian K. Kiragu <brian@amprest.co.ke>
2020-10-12 12:51:54 +0300

    Comments to prepare and check.
    Added retrieve.

--- app/Http/Controllers/Pay/C2B/MPesaController.php
+++ app/Http/Controllers/Pay/C2B/MPesaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Pay\C2B;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -18,6 +19,13 @@ class MPesaController extends Controller
         $this->headers = ['Api-Token' => env('AMPREST_PAYMENT_API_TOKEN')];
     }
 
+    /**
+     * Prepare a transaction for payment.
+     *
+     * @param Request $request
+     * @return Response
+     * @author Brian K. Kiragu <brian@amprest.co.ke>
+     */
     public function prepare(Request $request)
     {
         try {
@@ -29,14 +37,21 @@ class MPesaController extends Controller
             // Set the response.
             $payload = $response;
             $status = 201;
-        } catch (\Throwable $th) {
-            $payload = $th->getMessage();
+        } catch (Exception $e) {
+            $payload = $e->getMessage();
             $status = 404;
         } finally {
             return response($payload, $status);
         }
     }
 
+    /**
+     * Check if a transaction was completed.
+     *
+     * @param Request $request
+     * @return Response
+     * @author Brian K. Kiragu <brian@amprest.co.ke>
+     */
     public function check(Request $request)
     {
         try {
@@ -48,8 +63,34 @@ class MPesaController extends Controller
             // Set the response.
             $payload = $response;
             $status = 201;
-        } catch (\Throwable $th) {
-            $payload = $th->getMessage();
+        } catch (Exception $e) {
+            $payload = $e->getMessage();
+            $status = 404;
+        } finally {
+            return response($payload, $status);
+        }
+    }
+
+    /**
+     * Mark a transaction as retrieved.
+     *
+     * @param array $data
+     * @return Response
+     * @author Brian K. Kiragu <brian@amprest.co.ke>
+     */
+    protected function retrieve(array $data)
+    {
+        try {
+            // Send the request to the service.
+            $response = Http::withHeaders($this->headers)
+                ->post("$this->uri/mobile-money/safaricom/c2b/retrieve", $data)
+                ->json();
+
+            // Set the response.
+            $payload = $response;
+            $status = 200;
+        } catch (Exception $e) {
+            $payload = $e->getMessage();
             $status = 404;
         } finally {
             return response($payload, $status);
