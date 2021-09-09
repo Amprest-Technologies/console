db53271bfbe60fc242e1cd867fc1a34c0b369035
Alvin Kaburu <geekaburu@amprest.co.ke>
2021-09-09 19:02:11 +0300

    Replaced hook

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -131,7 +131,7 @@ class UserSeeder extends Seeder
             'name' => 'Wambuafrikan Global',
             'description' => 'Management of the wambuafrikan brand',
             'pay_callback' => null,
-            'pay_validation_hook' => 'https://google.com',
+            'pay_validation_hook' => 'https://nyumbanitech.co.ke/he',
             'uuid' => '10000004',
             'api_key' => 'e28d77fe328fd26f9f1d7afe6d4e2d47',
         ]);
